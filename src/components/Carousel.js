7e66d76f241dd94e0ad458a5cfef339e8daedac4
Albs1de <dkoskovik@gmail.com>
2023-05-12 14:59:03 +0200

    Fix the Carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,25 +1,17 @@
-import Carousel from "react-bootstrap/Carousel";
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "../styles/Carousel.css";
+import { Carousel } from "react-responsive-carousel";
 
 function CarouselFadeExample({ movies }) {
   return (
-    <Carousel fade>
-      {movies ? (
-        movies.map((movie, index) => (
-          <Carousel.Item key={index}>
-            <img
-              className="d-block w-100"
-              src={movie.Poster}
-              alt={movie.Title}
-            />
-            <Carousel.Caption>
-              <h3>{movie.Title}</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        ))
-      ) : (
-        <p>Loading...</p>
-      )}
+    <Carousel autoPlay infiniteLoop showThumbs={false} showIndicators={false}>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <img className="w-25 h-50" src={movie.Poster} alt={movie.Title} />
+          <p className="legend">{movie.Title}</p>
+        </div>
+      ))}
     </Carousel>
   );
 }
