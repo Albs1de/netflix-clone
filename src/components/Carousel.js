65d354c4d2bb8ea7aa32753832fcf29c32338a2b
Albs1de <dkoskovik@gmail.com>
2023-05-15 10:01:26 +0200

    Responsives Design für Carousel erstellen

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -13,6 +13,29 @@ function CarouselFade({ movieOrTVShow }) {
     slidesToScroll: 5,
     autoplay: true,
     autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
